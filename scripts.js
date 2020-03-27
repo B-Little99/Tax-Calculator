16cfe52348995f4070bf7015ad497afa143537c6
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-27 12:09:31 +0000

    Added JavaScript

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ function lowerTax() {
 
 function higherRateTax() {
     let higherTaxIncome = (income - lowerTaxBracket) * 0.60; //This gets the taxable income value for the higher rate taxes and multiplies it by 0.60 to get the taxed income for the higher rate bracket.
-    let finalHigherTax = higherTaxIncome + lowerTax(50000);
+    let finalHigherTax = higherTaxIncome + (lowerTax(50000));
     return finalHigherTax;
 }
 
@@ -26,7 +26,7 @@ function additionalRateTax() {
 function yearlyCalc() {
     let answer;
     if (income < personalAllowance) {
-            answer = "You do not pay any tax!"
+        document.getElementById("output").innerHTML = "You do not pay any tax!"
        } else if ( income > lowerTaxBracket && income < higherRateBracket) {
             answer = higherRateTax();
             return answer;
@@ -38,21 +38,24 @@ function yearlyCalc() {
             return answer;
         }
     // answer = parseFloat(answer).toFixed(2);
-    document.getElementById("output").innerHTML = "You takehome £" + answer + "p every year!";
+    document.getElementById("output").innerHTML = "You takehome £" + answer + " every year!";
 }
 
 
 function monthlyCalc() {
     answer = (yearlyCalc()) / 12;
-    document.getElementById("output").innerHTML = "You takehome £" + answer.toFixed(2) + "p every year!";
+    document.getElementById("output").innerHTML = "You takehome £" + answer.toFixed(2) + " every year!";
 }
 
 function finalCalc() {
     if (document.getElementById("yearly").checked) {
         yearlyCalc();
-    } else {
+    } else if (document.getElementById("monthly").checked) {
         monthlyCalc()
     }
+        else {
+            document.getElementById("output").innerHTML = "Please add your salary before hitting submit";
+        }
 }
 
 function reset() {
