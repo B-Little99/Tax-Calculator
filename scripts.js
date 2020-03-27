4c34dbc573419db5cea575301271d7384d66a8ad
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-27 21:07:09 +0000

    Fixed minor issues with JavaScript, started the CSS and made adjustments to the HTML for CSS.

--- scripts.js
+++ scripts.js
@@ -4,9 +4,9 @@ let lowerTaxBracket = 50000;
 let higherRateBracket = 150000;
 
 function lowerTax() {
-    let lowerTaxableIncome;
-    let finalLower;
-    if (income > 150000) {
+    let lowerTaxableIncome; 
+    let finalLower; 
+    if (income > 50000) {
         lowerTaxableIncome = (50000 - personalAllowance) * 0.80;
         finalLower = lowerTaxableIncome + personalAllowance;
         return finalLower;
@@ -22,21 +22,19 @@ function higherRateTax() {
     let finalHigherTax;
     if (income > 150000) {
         higherTaxIncome = 100000 * 0.60; // This gets the taxable income for the additional rate tax payers as they will always need to calculate the 100000 (the money for this bracket) as they have over 150k in yearly income.
-        finalHigherTax = higherTaxIncome + (lowerTax(50000));
+        finalHigherTax = higherTaxIncome + (lowerTax());
         return finalHigherTax;
     }
     else {
         higherTaxIncome = (income - lowerTaxBracket) * 0.60; //This gets the taxable income value for the higher rate taxes and multiplies it by 0.60 to get the taxed income for the higher rate bracket.
-        finalHigherTax = higherTaxIncome + (lowerTax(50000));
+        finalHigherTax = higherTaxIncome + (lowerTax());
         return finalHigherTax;
     }
 }
 
-
-
 function additionalRateTax() {
     let additionalRateTaxIncome = (income - higherRateBracket) * 0.55;
-    let finaladditionalRateTax = additionalRateTaxIncome + higherRateTax(100000); //+ lowerTax(50000);
+    let finaladditionalRateTax = additionalRateTaxIncome + higherRateTax();
     return finaladditionalRateTax;
 }
 
@@ -52,10 +50,9 @@ function yearlyCalc() {
             answer = additionalRateTax();
     } else {
         document.getElementById("output").innerHTML = "There has been an error.";
-        return; // stops rest of function
+        return;
     }
-    // answer = parseFloat(answer).toFixed(2);
-    let outputstring =  "You take home £" + answer.toString() + " every year"; //practice this more
+    let outputstring =  "You take home £" + answer.toString() + " every year";
     document.getElementById("output").innerHTML = outputstring;
     return answer;
 }
@@ -63,7 +60,7 @@ function yearlyCalc() {
 function monthlyCalc() {
     let answer;
     answer = (yearlyCalc()) / 12;
-    document.getElementById("output").innerHTML = "You takehome £" + answer.toFixed(2) + " every month!";
+    document.getElementById("output").innerHTML = "You take home £" + answer.toFixed(2) + " every month!";
 }
 
 function finalCalc() {
